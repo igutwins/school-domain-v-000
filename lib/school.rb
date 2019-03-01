89f5d4ae9b3e62ca776cfa0b9ac10c1a92f98878
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-01 02:51:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,10 @@
 # code here!
 class School
+  roster = {}
   def initialize(name)
     @name = name
-    roster = {}
+  end
+  def add_student(student, grade)
+    roster << grade => student 
   end
 end
