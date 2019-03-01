d1211bedd749780142e157afb728b337605bef1a
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-01 03:01:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,11 @@ class School
   end
 
   def add_student(student, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else 
     @roster[grade] = []
     @roster[grade] << student
+  end 
   end
 end
